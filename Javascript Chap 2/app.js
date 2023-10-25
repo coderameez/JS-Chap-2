79c740b93337ff6b9dfa49898fbd398e063e2d88
Muhammad Rameez Khan <123770847+rameezkhan77@users.noreply.github.com>
2023-10-25 18:34:22 +0500

    Add files via upload

---
+++ Javascript Chap 2/app.js
@@ -0,0 +1,37 @@
+// Chapter 2 //
+// Question 1 //
+
+
+var username;
+var myName = "Muhammad Rameez Khan"
+console.log(myName)
+
+var message = "Hello World"
+alert(message);
+
+var name = "John Doe"
+var age = "15 years old"
+var cert = "Certified Mobile App Developer"
+alert(name)
+alert(age)
+alert(cert)
+
+var pizza = "pizza\npizz\npiz\npi\np";
+alert(pizza);
+
+var email = "m.rameezkhan77@gmail.com"
+alert("my email address is" + " " + email)
+
+
+var book = "A smarter way to learn javascript"
+alert("I am trying to learn from the book" + " " + book)
+
+document.write("Yah! I can write HTML cotent through JS");
+
+
+var specialString = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+
+
+alert(specialString);
+
+document.write(specialString);
